4afd5dbe34c59820b1cdd65f4c85d5eb67c06d8a
bianco-e <bianco.emiliozzi@gmail.com>
2021-04-28 15:46:05 -0300

    Update PC cards for campaign

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,11 @@
 module.exports = {
   campaignPcAnimals: {
     0: ["Mosquito", "Bee", "Parrot", "Toad", "Salamander"],
-    300: ["Mosquito", "Bee", "Parrot", "Toad", "Vulture"],
-    600: ["Tortoise", "Bee", "Parrot", "Toad", "Vulture"],
-    1000: ["Salamander", "Bee", "Spider", "Toad", "Electric Eel"],
+    300: ["Mosquito", "Bee", "Parrot", "Toad", "Hedgehog"],
+    600: ["Tortoise", "Bee", "Parrot", "Toad", "Swordfish"],
+    1000: ["Salamander", "Bee", "Spider", "Toad", "Frog"],
     2000: ["Mosquito", "Bee", "Spider", "Scorpion", "Salamander"],
-    3000: ["Eagle", "Cassowary", "Parrot", "Vulture", "Salamander"],
+    3000: ["Eagle", "Cassowary", "Parrot", "Ostrich", "Salamander"],
     4000: ["Orc", "Shark", "Stingray", "Blowfish", "Mosquito"],
     5000: ["Chameleon", "Crocodile", "Komodo Dragon", "Snake", "Tortoise"],
     6000: ["Lion", "Elephant", "Gorilla", "Cheetah", "Hyena"],
