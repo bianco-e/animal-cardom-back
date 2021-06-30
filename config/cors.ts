c148b26bbb171a9c31742308d2a28b2f60e97002
bianco-e <bianco.emiliozzi@gmail.com>
2021-06-29 21:24:54 -0300

    Add origins whitelist to cors config gile

--- config/cors.ts
+++ config/cors.ts
@@ -1,20 +1,16 @@
 import { CorsOptions } from "cors";
 
-/* const CORS_WHITE_LIST: string[] = [
-  "http://localhost:3000/",
-  "https://animalcardom.vercel.app/",
+const CORS_WHITE_LIST: string[] = [
+  "http://localhost:3000",
+  "https://animalcardom.vercel.app",
 ];
 
 const CORS_CONFIG: CorsOptions = {
-  origin: (origin: undefined | string, callback: any) => {
-    if ((origin && CORS_WHITE_LIST.indexOf(origin) !== -1) || !origin) {
+  origin: (origin: string | undefined, callback: any) => {
+    if (CORS_WHITE_LIST.includes(origin!)) {
       callback(null, true);
     } else callback(new Error("Not allowed by CORS"));
   },
-}; */
-
-const CORS_CONFIG: CorsOptions = {
-  origin: "https://animalcardom.vercel.app",
 };
 
 export default CORS_CONFIG;
